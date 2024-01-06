9b0ba78a3d43439a8c4ed732647826f44cba9a82
Duncan Champney <DChampney@WareTo.com>
2024-01-06 14:09:38 -0500

    Initial Commit

---
+++ TabViewTest/TabViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTestApp.swift
+//  TabViewTest
+//
+//  Created by Duncan Champney on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
